9faff2a7a977b70b01f4ab4f5ea3496ec260156a
thanvanhai <thanvanhai1021988@gmail.com>
2026-07-21 07:16:47 +0700

    fix remove ocs/intro.md

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -116,11 +116,7 @@ const config: Config = {
       links: [
         {
           title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
+          items: [           
           ],
         },
         {
